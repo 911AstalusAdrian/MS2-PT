1c4333e188ba62405865aff60048f13255218fb4
Adrian Astalus <astalus.adrian1703@gmail.com>
2024-03-07 13:49:38 +0200

    Initial page structure + JSON information add

---
+++ A1/js/details.js
@@ -0,0 +1,16 @@
+var personalDetails = {
+    'firstName' : 'Adrian',
+    'lastName' : 'Astalus',
+    'dateOfBirth' : '17-03-2002', // date type maybe?
+    'placesLived1' : 'Ludus',
+    'placesLived2' : 'Cluj-Napoca'
+}
+
+var professionalDetails = {
+    'education1' : 'Technological High School',
+    'educationPlace1' : 'Ludus',
+    'education2' : 'Bachelor of Computer Science',
+    'edicationPlace2' : 'Babes-Bolyai University in Cluj-Napoca',
+    'education3' : 'Data Science for Society and Industry Masters Program',
+    'educationPlace3' : 'Babes-Bolyai University in Cluj-Napoca'
+}
